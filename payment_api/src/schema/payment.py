0b3abe36558215eb17129117d69daee471f8ceed
IAmIrina <irina.vl.kiseleva@gmail.com>
2022-11-15 12:21:41 +0000

    Add webhook

--- payment_api/src/schema/payment.py
+++ payment_api/src/schema/payment.py
@@ -1,4 +1,9 @@
+"""Ecom models."""
+
 from enum import Enum
+from typing import Union
+
+from schema.mixins import OrjsonModel
 
 
 class RefundReason(Enum):
@@ -6,9 +11,37 @@ class RefundReason(Enum):
     fraudulent = 'Fraud'
     requested_by_customer = 'Requested'
 
+    class Config:
+        orm_mode = True
+
 
 class Event(Enum):
-    charge_succeeded = 'charge.succeeded'
     checkout_session_completed = 'checkout.session.completed'
     checkout_session_expired = 'checkout.session.expired'
     charge_refunded = 'charge.refunded'
+
+    class Config:
+        orm_mode = True
+
+
+class CompletedSession(OrjsonModel):
+    session_id: str
+    payment_intent: str
+
+    class Config:
+        orm_mode = True
+
+
+class RefundedCharge(OrjsonModel):
+    charge_id: str
+    payment_intent: str
+
+
+class ExpiredSession(OrjsonModel):
+    session_id: str
+
+
+class PaymentEvent(OrjsonModel):
+    type: Event
+    data: Union[CompletedSession, ExpiredSession, RefundedCharge]
+    row_data: dict
--- payment_api/src/schema/product.py
+++ payment_api/src/schema/product.py
@@ -1,22 +1,8 @@
-"""Pydantic models."""
+"""Product models."""
 
 from typing import Literal
 
-import orjson
-from pydantic import BaseModel
-
-
-def orjson_dumps(v, *, default) -> str:
-    """Fast json dumps."""
-    return orjson.dumps(v, default=default).decode()
-
-
-class OrjsonModel(BaseModel):
-    """Fast json serializer."""
-
-    class Config:
-        json_loads = orjson.loads
-        json_dumps = orjson_dumps
+from schema.mixins import OrjsonModel
 
 
 class ProductData(OrjsonModel):
