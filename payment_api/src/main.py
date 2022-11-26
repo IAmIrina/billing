e6ff5933e26c1b2aac096fe1931db2cadedfc9b0
petrovskyd <petrovsky.dv@gmail.com>
2022-11-26 15:03:30 +0600

    fix format

--- payment_api/src/main.py
+++ payment_api/src/main.py
@@ -3,7 +3,6 @@ import sentry_sdk
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-
 from api.v1 import payments, subscriptions, refunds, webhook
 from core.config import settings
 from ecom import abstract, event_listeners, stripe_api
@@ -41,7 +40,6 @@ app.include_router(payments.router, prefix='/api/v1/payments', tags=['payments']
 app.include_router(subscriptions.router, prefix='/api/v1/subscriptions', tags=['subscriptions'])
 app.include_router(refunds.router, prefix='/api/v1/refunds', tags=['refunds'])
 app.include_router(webhook.router, prefix='/api/v1/webhook', tags=['webhook'])
-
 
 if __name__ == '__main__':
     uvicorn.run(
